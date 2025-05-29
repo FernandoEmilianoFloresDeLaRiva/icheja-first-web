71a770273a22ebf5e6395edcd0c851dddb5c8664
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2025-05-28 21:27:08 -0600

    Responsive

--- src/common/components/HeaderLogo/HeaderLogo.tsx
+++ src/common/components/HeaderLogo/HeaderLogo.tsx
@@ -1,15 +1,34 @@
+import Grid from '@mui/material/Grid';
 import divider from "../../../assets/images/divider.png";
 import secondaryLogo from "../../../assets/images/secondary-logo.png";
 
 export default function HeaderLogo() {
   return (
-    <div className="flex justify-between h-39">
-      <img src={divider} alt="Icon divider" className="h-10 w-250 mt-10" />
-      <img
-        src={secondaryLogo}
-        alt="Secondary Logo"
-        className="h-16 w-auto object-contain mt-9"
-      />
-    </div>
+      <Grid container spacing={2} direction={{ xs: 'column', sm: 'row' }}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: "15px",
+          marginBottom: "15px"
+        }}>
+        <Grid size={{ xs: 12, sm: 8, md: 8, lg : 10 }}>
+          <img src={divider} 
+               alt="Icon divider" 
+               className="h-10" 
+               style={{
+                  width: 'auto',
+                  minWidth: '60px', 
+                  maxWidth: '100%',
+                  objectFit: 'cover',
+                  objectPosition: 'left' 
+                }}/>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 4, md: 4, lg : 2 }}>
+          <img
+            src={secondaryLogo}
+            alt="Secondary Logo"
+          />
+        </Grid>
+      </Grid>
   );
 }
